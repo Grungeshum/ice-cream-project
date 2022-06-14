a1d2dc78a37ec4465fd61a3fab85542954faca1d
Grungeshum <62945459+Grungeshum@users.noreply.github.com>
2022-06-14 06:58:07 +0000

    Deploying to gh-pages from @ Grungeshum/ice-cream-project@7b9e9a2b7d330274a34cc6e55228147bf327dd88 🚀

---
+++ index.be97f80d.js
@@ -0,0 +1,2 @@
+(()=>{const o={openMobModalBtn:document.querySelector("[data-mob-modal-open]"),modal:document.querySelector("[data-mob-modal]")};o.openMobModalBtn.addEventListener("click",(function(){o.modal.classList.toggle("is-hidden")}))})();
+//# sourceMappingURL=index.be97f80d.js.map
