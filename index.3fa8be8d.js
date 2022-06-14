d1383036a859f1427b928cbdb7578b3834f680e2
Grungeshum <62945459+Grungeshum@users.noreply.github.com>
2022-06-14 20:58:11 +0000

    Deploying to gh-pages from @ Grungeshum/ice-cream-project@5bb9caf6fbd795c988dc6207b61ba6ec2d796bad 🚀

--- index.3fa8be8d.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;(o={openMobModalBtn:document.querySelector("[data-mob-modal-open]"),modal:document.querySelector("[data-mob-modal]")}).openMobModalBtn.addEventListener("click",(function(){o.modal.classList.toggle("is-hidden")}))}();
-//# sourceMappingURL=index.3fa8be8d.js.map
---
+++ index.a039f7db.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({dots:!0,arrows:!1,autoplay:!0,sliderToShow:3,autoplaySpeed:2e3,centerMode:!1,speed:600,responsive:[{breakpoint:1280},{breakpoint:768},{breakpoint:320}]})}));
+//# sourceMappingURL=index.a039f7db.js.map
